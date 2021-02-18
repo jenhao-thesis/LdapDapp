dd527f8d7c7f88d0d9ac1c9aa957d9066f07bf6f
ren-hao <60211tom@gmail.com>
2021-02-18 15:47:38 +0800

    fix data sharing get token

--- routes/dataSharing.js
+++ routes/dataSharing.js
@@ -39,62 +39,76 @@ router.get('/', isAuthenticated, async function(req, res) {
 });
 
 router.get('/getAccessToken', isAuthenticated, async function(req, res) {
-    // TODO: lookup which org we should ask
-    let provider_address = "0xA3e898C280220bf5fAE9e7e6ceB4F3a6BFa67163";
-    let provider_ip = "192.168.139.133:3000";
-    let jwt = "";
-    let identity = req.user.hashed;
-    let signatureObject;
-    let nonceObject;
-    // prove org identity, it should be nonce from provider
-    await fetch(`http://${provider_ip}/users/auth/nonce?org=${admin_address}`)
-        .then( res => res.json())
-        .then( json => {
-            console.log(json);
-            nonceObject = json;
-        });
-    signatureObject =  web3.eth.accounts.sign(nonceObject.nonce, admin_key);
-
-    // get token
-    await fetch(`http://${provider_ip}/users/authenticate`,{ 
-            method: 'POST',
-            body: JSON.stringify({
-                identity: identity,
-                target_address: admin_address,
-                signature: signatureObject,
-                nonce: nonceObject
-            }),
-            headers: {'Content-Type': 'application/json'}
-        })
-        .then( res => res.json())
-        .then( json => {
-            if (!json.success) return res.send({status: false, message: json.message});
-            jwt = json.token
-        })
-        .catch( (err) => {
-            console.log(err);
-        });
-    
-    if (jwt) {
-
-        const token = {
-            identity: identity,
-            org: provider_address,
-            jwt: jwt
-        }
-    
-        await db.tokens.upsert(token)
-            .then(data => {
-                console.log("upsert successfully");
-            })
-            .catch(err => {
-                console.log(err.message || "Some error occurred while creating the Token")
-            });
-
-
-        return res.send({status: true, message:"Got token successfully."});
+    let {provider_address} = req.query;
+    if (!provider_address)
+        return res.json({msg: "address of provider is missing."});
+    provider_address = '0x' + provider_address.substr(2, provider_address.length-2);
+    if (config.org_mapping[provider_address] == null) {
+        return res.send({status: false, message: "provider ip is not found"});
     }
-    return res.send({status: false, message: "org has no response."});
+    else {
+        let provider_ip = config.org_mapping[provider_address];
+        let jwt = "";
+        let identity = req.user.hashed;
+        let signatureObject;
+        let nonceObject;
+        // prove org identity, it should be nonce from provider
+        await fetch(`http://${provider_ip}/users/auth/nonce?org=${admin_address}`)
+            .then( res => res.json())
+            .then( json => {
+                console.log(json);
+                nonceObject = json;
+            })
+            .catch( (err) => {
+                console.log(err);
+                return res.send({status: false, message: err.code});
+            });
+        signatureObject =  web3.eth.accounts.sign(nonceObject.nonce, admin_key);
+
+        // get token
+        await fetch(`http://${provider_ip}/users/authenticate`,{ 
+                method: 'POST',
+                body: JSON.stringify({
+                    identity: identity,
+                    target_address: admin_address,
+                    signature: signatureObject,
+                    nonce: nonceObject
+                }),
+                headers: {'Content-Type': 'application/json'}
+            })
+            .then( res => res.json())
+            .then( json => {
+                if (!json.success) return res.send({status: false, message: json.message});
+                jwt = json.token
+            })
+            .catch( (err) => {
+                console.log(err);
+                return res.send({status: false, message: err.code});
+            });
+        
+        if (jwt) {
+
+            const token = {
+                identity: identity,
+                org: provider_address,
+                jwt: jwt
+            }
+        
+            await db.tokens.upsert(token)
+                .then(data => {
+                    console.log("upsert successfully");
+                })
+                .catch(err => {
+                    console.log(err.message || "Some error occurred while creating the Token")
+                });
+
+
+            return res.send({status: true, message:"Got token successfully."});
+        }
+        return res.send({status: false, message: "org has no response."});        
+    }
+
+
 });
 
 module.exports = router;
