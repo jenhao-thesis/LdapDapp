3320864fe7e1375ce160309210cb2365ff8405b4
ren-hao <60211tom@gmail.com>
2021-05-06 17:35:29 +0800

    fix send signed tx

--- routes/metamask-connect.js
+++ routes/metamask-connect.js
@@ -5,7 +5,7 @@ const Web3 = require('web3');
 const util = require('util');
 const ldap = require('ldapjs');
 
-const config = JSON.parse(fs.readFileSync('./server-config.json', 'utf-8'));    
+const config = JSON.parse(fs.readFileSync('./server-config.json', 'utf-8'));
 const web3 = new Web3(new Web3.providers.WebsocketProvider(config.web3_provider));
 const admin_address = config.admin_address; // org0
 const contract_address = config.contracts.organizationManagerAddress;
@@ -13,25 +13,24 @@ const client = ldap.createClient(config.ldap.server);
 
 let defaultDN = "cn=%s,ou=location2,dc=jenhao,dc=com";
 
-router.get('/', function(req, res) {
+router.get('/', function (req, res) {
     console.log("web3 version", web3.version);
-    res.render('metamask-connect', { title: 'Metamask connection',
-                                     address: contract_address});
+    res.render('metamask-connect', {
+        title: 'Metamask connection',
+        address: contract_address
+    });
 });
 
-router.get('/org.json', function(req, res) {
+router.get('/org.json', function (req, res) {
     let contract = JSON.parse(fs.readFileSync('./build/contracts/OrganizationManager.json', 'utf-8'));
     res.json(contract);
 });
 
-router.post('/addUser', async function(req, res, next) {
-    console.log("web3 version", web3.version);
-    await web3.eth.personal.unlockAccount(admin_address, "12345678", 15000);
-    // web3.eth.defaultAccount = admin_address;
-    let contract = JSON.parse(fs.readFileSync('./build/contracts/OrganizationManager.json', 'utf-8'));    
+router.post('/addUser', async function (req, res, next) {
+    let contract = JSON.parse(fs.readFileSync('./build/contracts/OrganizationManager.json', 'utf-8'));
     let contractInstance = new web3.eth.Contract(contract.abi, contract_address);
     let txHash;
-    
+
     // await web3.eth.getBalance(admin_address).then(function(result) {
     //     balance = result;
     // })
@@ -60,35 +59,57 @@ router.post('/addUser', async function(req, res, next) {
 
     // TODO: addUser
     let userId = req.body.uid;
-    
-    let DN = util.format(defaultDN, req.body.cn); 
+
+    let DN = util.format(defaultDN, req.body.cn);
     let change = {
         operation: 'replace',
         modification: {
             idStatus: 1
         }
     };
-    
-    client.modify(DN, change, async function(err) {
+
+    // 對要發送出去的交易做簽名
+    let signedTxObj;
+    let tx_builder = contractInstance.methods.addUser(userId);
+    let encode_tx = tx_builder.encodeABI();
+    let transactionObject = {
+        gas: 6721975,
+        data: encode_tx,
+        from: admin_address,
+        to: contract_address
+    }
+    await web3.eth.accounts.signTransaction(transactionObject, config.admin_key, async function (error, signedTx) {
+        if (error) {
+            console.log("sign error");
+        } else {
+            signedTxObj = signedTx;
+        }
+    })
+
+
+
+    client.modify(DN, change, async function (err) {
         if (err !== null) {
             console.log("error in modify");
-            return res.send({msg: "Common name is invalid."});
-        }
-        else {
-            await contractInstance.methods.addUser(userId).send({
-                from: admin_address,
-                gas: 6721975
-            }, function(error, transactionHash) {
-                if (error) {
-                    console.log("err", error);
-                }
-                else {
-                    console.log("Transaction hash:", transactionHash);
-                    txHash = transactionHash;
-                }
-            })
-
-            return res.send({msg: `${req.body.uid}-${txHash}`});
+            return res.send({
+                msg: "Common name is invalid."
+            });
+        } else {
+            // 發送交易
+            web3.eth.sendSignedTransaction(signedTxObj.rawTransaction)
+                .on('receipt', function (receipt) {
+                    console.log(receipt);
+                    return res.send({
+                        msg: `${req.body.uid}-${receipt.transactionHash}`
+                    });
+                })
+                .on('error', function (error) {
+                    console.log(`Send signed transaction failed.`);
+                    console.log(error)
+                    return res.status(500).send({
+                        msg: "error"
+                    });
+                })
         }
     });
 
@@ -105,8 +126,8 @@ router.post('/addUser', async function(req, res, next) {
     // });
 });
 
-router.post('/bindAccount', async function(req, res, next) {
-    let contract = JSON.parse(fs.readFileSync('./build/contracts/OrganizationManager.json', 'utf-8'));    
+router.post('/bindAccount', async function (req, res, next) {
+    let contract = JSON.parse(fs.readFileSync('./build/contracts/OrganizationManager.json', 'utf-8'));
     let contractInstance = new web3.eth.Contract(contract.abi, contract_address);
 
     let userId = req.body.uid;
@@ -117,17 +138,18 @@ router.post('/bindAccount', async function(req, res, next) {
 
     await contractInstance.methods.bindAccount(userId, userAddress).send({
         from: admin_address
-    }, function(error, transactionHash) {
+    }, function (error, transactionHash) {
         if (error) {
             console.log("err", error);
-        }
-        else {
+        } else {
             txHash = transactionHash;
             console.log("Transaction hash:", transactionHash);
         }
     })
 
-    res.send({msg: "OK, i got it, this is your transaction hash:" + txHash});
+    res.send({
+        msg: "OK, i got it, this is your transaction hash:" + txHash
+    });
 });
 
 module.exports = router;
